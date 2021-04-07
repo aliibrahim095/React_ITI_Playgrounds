cd186d948e90564b19bf94d5773128fdb3e7b306
aliibrahim095 <4liebrahim@gmail.com>
2021-04-07 20:08:14 +0200

    seconds visible if minutes greter than zero

--- src/Content.jsx
+++ src/Content.jsx
@@ -42,13 +42,14 @@ class Content extends Component {
             <div className="container mt-5">
                 <div className="row mb-5">
                     <div className="col-12 alert bg-dark text-light text-center p-5">
-                        <button  disabled={this.state.count===5}  onClick={this.increaseCounter} className="btn btn-primary p-4 mr-2">+</button>
+                        <button  disabled={this.state.count===1||this.state.count===0} onClick={this.decrementCounter}  className="btn btn-danger p-4 mr-2">-</button>
                         <b className="alert alert-primary p-5 mr-2"><span className="badge badge-warning p-2 mr-1">{this.state.count}</span>{this.state.count===1 ? 'Minute':'Minutes'} </b>
-                        <button  disabled={this.state.count===1} onClick={this.decrementCounter}  className="btn btn-danger p-4 ">-</button>
+                        <button  disabled={this.state.count===5}  onClick={this.increaseCounter} className="btn btn-primary p-4">+</button>
+
                     </div>
                 </div>
                 <div className="row">
-                    <b className="col-12 alert alert-primary p-5 mr-2 text-center mb-5"><span className="badge badge-warning p-2 mr-1">{this.state.seconds}</span> {this.state.seconds===1 ? 'Second':'Seconds'}</b>
+                   {this.state.count>0 && <b className="col-12 alert alert-primary p-5 mr-2 text-center mb-5"><span className="badge badge-warning p-2 mr-1">{this.state.seconds}</span> {this.state.seconds===1 ? 'Second':'Seconds'}</b>}
                 </div>
             </div>
         );
